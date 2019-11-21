83bc0c7c2e2f067e29aa085a4b00b8b695d7e733
Will McGill <mcgill.will@yahoo.com>
2019-11-21 14:45:10 -0500

    added regestration functionality

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -35,6 +35,45 @@ class AuthenticationController extends Controller
         $response = 'You have been successfully logged out!';
         return response($response, 200);
     }
+
+   
+    function register(Request $request)
+    {
+        /**
+         * Get a validator for an incoming registration request.
+         *
+         * @param  array  $request
+         * @return \Illuminate\Contracts\Validation\Validator
+         */
+        $valid = validator($request->only('email', 'name', 'password'), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($valid->fails()) {
+            $jsonError=response()->json($valid->errors()->all(), 400);
+            return \Response::json($jsonError);
+        }
+        $data = request()->only('email','name','password');
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+        if ($user) {
+            if ($user->validateForPassportPasswordGrant($request->password) == $user->password) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = 'Password mismatch';
+                return response($response, 422);
+            }
+        } else {
+            $response = 'User doesn\'t exist';
+            return response($response, 422);
+        }
+    }
 }
 
 
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,12 @@ use Illuminate\Http\Request;
 */
 Route::post('/login', 'AuthenticationController@login')->name('login');
 
-
+Route::post('/register' , 'AuthenticationController@register')->name('register');
 
 Route::middleware('auth:api')->group(function(){ 
     Route::get('/logout', 'AuthenticationController@logout')->name('logout');
+    
+    
 }) ;
     
     
