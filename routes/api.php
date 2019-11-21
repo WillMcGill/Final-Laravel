10eeab270e3a1cbcc775b245fca7543bb0d92ef3
Will McGill <mcgill.will@yahoo.com>
2019-11-21 09:00:07 -0500

    created log out functionality

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Http\Request;
 */
 Route::post('/login', 'AuthenticationController@login')->name('login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::middleware('auth:api')->group(function(){ 
+    Route::get('/logout', 'AuthenticationController@logout')->name('logout');
+}) ;
+    
+    
+
