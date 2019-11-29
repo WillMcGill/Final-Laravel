eceb6196322aa6490cb0556790dbdd216c471d1e
Will McGill <mcgill.will@yahoo.com>
2019-11-29 12:49:07 -0500

    changed seed on routes to minus 9 weeks and plus 3 weeks'

--- database/factories/RouteFactory.php
+++ database/factories/RouteFactory.php
@@ -7,14 +7,13 @@ use Faker\Generator as Faker;
 
 
 $factory->define(Routes::class, function (Faker $faker) {
-    static $number = 0;
 
     return [
         'wall_location' => $faker->unique()->numberBetween($min = 1, $max = 75),
         'type' => $faker->randomElement(['Top Rope', 'Lead', 'Auto-Belay']),
         'difficulty' => $faker->randomElement(['5.7','5.8', '5.9', '5.10', '5.11', '5.12', '5.13']),
-        'set_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
-        'expire_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'set_date' => $faker->dateTimeBetween($startDate = '- 9 weeks', $endDate = 'now', $timezone = null),
+        'expire_date' => $faker->dateTimeBetween($startDate = 'now', $endDate = '+ 3 weeks', $timezone = null),
         'active' => true
     ];
 });
